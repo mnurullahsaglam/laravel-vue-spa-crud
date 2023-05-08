2bad29a54b4ada89696a05a9bf8943068e4fb6e5
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 00:24:15 +0300

    create seeder for users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@localhost',
+            'password' => bcrypt('admin'),
+        ]);
+
+        User::factory()->count(10)->create();
+    }
+}
