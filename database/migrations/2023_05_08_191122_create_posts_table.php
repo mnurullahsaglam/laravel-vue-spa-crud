ee03990a0669a68b247d4d8b52c18528f77992a0
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 01:08:07 +0300

    add category_id to posts migration

--- database/migrations/2023_05_08_191122_create_posts_table.php
+++ database/migrations/2023_05_08_191122_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->longText('content');
             $table->timestamps();
