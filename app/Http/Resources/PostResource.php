e5ebdddb7c1fef12eaf95e7c6674cb0f69365695
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 00:56:11 +0300

    update post resource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,11 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => substr($this->content, 0, 50) . '...',
+            'created_at' => $this->created_at->toDateString()
+        ];
     }
 }
