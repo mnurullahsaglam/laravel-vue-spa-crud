72c14b4383d6d7c0470d7ef183b9dbd4e496c197
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 01:07:46 +0300

    create seeder for categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
-class PostSeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Post::factory()->count(100)->create();
+        Category::factory()->count(10)->hasPosts(7)->create();
     }
 }
