a1a0e1d728bb7d3167877dd52c82323e0d18b1c4
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 00:55:20 +0300

    update posts.index method

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        return Post::all();
+        return PostResource::collection(Post::all());
     }
 
     public function create()
