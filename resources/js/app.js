c3f26dc4c1028acf9208c79fc92d5babb31adad8
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 02:23:14 +0300

    router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,9 @@
 import './bootstrap';
 
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './layouts/App.vue'
-import PostsIndex from './components/Posts/Index.vue'
 
-const routes = [
-    { path: '/', component: PostsIndex },
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+import router from './routes/index'
 
 createApp(App)
     .use(router)
---
+++ resources/js/routes/index.js
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+import PostsIndex from '@/components/Posts/Index.vue'
+import PostsCreate from '@/components/Posts/Create.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'posts.index',
+        component: PostsIndex,
+        meta: {title: 'Posts'}
+    },
+    {
+        path: '/posts/create',
+        name: 'posts.create',
+        component: PostsCreate,
+        meta: {title: 'Add New Post'}
+    },
+]
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+})
