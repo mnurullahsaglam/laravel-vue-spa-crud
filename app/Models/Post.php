4c278e06870173779c433068f4cc9ad9b7b68f31
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 01:08:19 +0300

    add category relation in post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'category_id',
         'title',
         'content'
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
