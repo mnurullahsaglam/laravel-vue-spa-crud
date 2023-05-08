3d47a37d8125edc9bc080296b05438c6380cffb0
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-08 21:42:41 +0300

    add id

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100" id="app">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
