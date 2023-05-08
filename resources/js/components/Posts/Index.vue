51864874d84c1d43b2514b6186b44a173f57c492
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-05-09 00:26:03 +0300

    update posts.index component

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -11,38 +11,52 @@
                         <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
+                        <span
+                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
                     </th>
                     <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>
+                        <span
+                            class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>
                     </th>
                 </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                <tr>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">1</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">A</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2022-01-01 13:43:47</td>
-                </tr>
-                <tr>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">B</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2022-01-02 14:43:47</td>
+                <tr v-for="post in posts">
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.id }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.title }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.content }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.created_at }}
+                    </td>
                 </tr>
                 </tbody>
             </table>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        this.fetchPosts()
+    },
+    methods: {
+        fetchPosts() {
+            axios.get('/api/posts')
+                .then(response => this.posts = response.data)
+                .catch(error => console.log(error))
+        }
+    }
+}
+</script>
